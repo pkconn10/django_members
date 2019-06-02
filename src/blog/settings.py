f66ffa54b7229e096b0f624a0ffd914e316e01f3
Kevin Connaughton <pkconnaught@yahoo.ie>
2019-06-02 16:52:59 +0800

    postgres DB and static root changes

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -84,10 +84,22 @@ WSGI_APPLICATION = "blog.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+if DEBUG:
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
+}
+else:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blogDB',
+        'USER': 'blog_admin',
+        'PASSWORD': 'oktober1',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
