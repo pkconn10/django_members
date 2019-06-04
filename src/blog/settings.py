6ad821eaa2c8ec8823266ba797ee6fc920dc957d
Kevin Connaughton <pkconnaught@yahoo.ie>
2019-06-04 17:29:07 +0800

    true

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "bx9d)qnb*=rn3upoam_u6bb-lh-)zoajni2*=gh_7)==gcwm2h"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['157.230.248.158']
 
